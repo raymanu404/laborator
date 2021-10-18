00a719f5240ac65c5fc79b1f5e2e2779745ac3cd
Emanuel Caprariu <manu.caprariu.brediceanu@gmail.com>
2021-10-18 09:11:28 +0300

    init tema3

---
+++ L03/Tema3_CaprariuEmanuel/Tema3_CaprariuEmanuel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tema3_CaprariuEmanuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
