a84e185e5a8cd5bda5f720657d98b2924a7ab371
Emanuel Caprariu <manu.caprariu.brediceanu@gmail.com>
2021-10-18 16:54:32 +0300

    tema3 facuta

--- L03/Tema3_CaprariuEmanuel/Tema3_CaprariuEmanuel/Program.cs
+++ L03/Tema3_CaprariuEmanuel/Tema3_CaprariuEmanuel/Program.cs
@@ -1,12 +1,83 @@
-﻿using System;
-
+﻿using Google.Apis.Drive.v3.Data;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Tema3_CaprariuEmanuel.home;
 namespace Tema3_CaprariuEmanuel
 {
     class Program
     {
+        private static string ID_DRIVE = "1E5B8jxfVAPZM_uikzm_3aBUMs5Oz-bm_";
+        private static DriveApiService driveApiService = driveApiService = new DriveApiService();
+        private static IList<Google.Apis.Drive.v3.Data.File> files;
+        
+        private static void menu()
+        {
+            
+            Console.WriteLine("1.Afisare fisiere");
+            Console.WriteLine("2.Creeare fisiere");
+            Console.WriteLine("3.Incarcare fisiere");
+            Console.WriteLine("0.Iesire");
+            Console.WriteLine("Alege optiune:");
+            Console.WriteLine("");
+        }
+        private static void afisare_fisiere()
+        {
+            files = driveApiService.ListEntities(ID_DRIVE);
+            Console.WriteLine("--------------------------");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+
+            Console.WriteLine("--------------------------");
+        }
+        private static void createDir()
+        {
+            Console.WriteLine("Dati numele folder pentru creeare:");
+            string dir = Console.ReadLine();
+            driveApiService.CreateFolder(dir, ID_DRIVE);
+            Console.WriteLine("Folder creat cu succes!");
+        }
+        private static async void uploadFile()
+        {
+            Console.WriteLine("Dati path-ul fisierului pentru a urca pe server:");
+            string path = Console.ReadLine();          
+            if(path != "")
+            {
+                var stream = new FileStream(path, FileMode.Open);
+               
+                await driveApiService.Upload(stream, ID_DRIVE);
+                Console.WriteLine("Incarcat cu succes!");
+            }
+           
+        }      
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opt;
+            do
+            {
+                menu();               
+                opt = Console.ReadLine();
+                Console.Clear();
+                switch (opt)
+                {                 
+                    case "1":
+                        afisare_fisiere();
+                        break;
+                    case "2":
+                        createDir();
+                        break;
+                    case "3":
+                        uploadFile();
+                        break;
+                 
+                }
+
+            } while (opt != "0");
+            
+
         }
     }
 }
---
+++ L03/Tema3_CaprariuEmanuel/Tema3_CaprariuEmanuel/home/DriveApiService.cs
@@ -0,0 +1,99 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tema3_CaprariuEmanuel.home
+{
+    public class DriveApiService
+    {
+        protected static string[] scopes = { DriveService.Scope.Drive };
+        protected readonly UserCredential credential;
+        static string ApplicationName = "DatcTema3";
+        protected readonly DriveService service;
+        protected readonly FileExtensionContentTypeProvider fileExtensionProvider;
+
+        public string YOUR_GMAIL_EMAIL { get; } = "manu.caprariu.brediceanu@gmail.com";
+
+        public DriveApiService()
+        {
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token1.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    YOUR_GMAIL_EMAIL, // use a const or read it from a config file
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+
+                fileExtensionProvider = new FileExtensionContentTypeProvider();
+            }
+
+            service = new DriveService(new BaseClientService.Initializer()
+
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+        public IList<Google.Apis.Drive.v3.Data.File> ListEntities(string id)
+        {
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 100;
+            listRequest.Fields = "nextPageToken, files(id, name, parents, createdTime, modifiedTime, mimeType)";
+            listRequest.Q = $"'{id}' in parents";
+
+            return listRequest.Execute().Files;
+        }
+        public Google.Apis.Drive.v3.Data.File CreateFolder(string name, string id)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = name,
+                MimeType = "application/vnd.google-apps.folder",
+                Parents = new[] { id }
+            };
+
+            var request = service.Files.Create(fileMetadata);
+            request.Fields = "id, name, parents, createdTime, modifiedTime, mimeType";
+
+            return request.Execute();
+        }
+        public async Task<Google.Apis.Drive.v3.Data.File> Upload(FileStream file, string documentId)
+        {
+            string[] file_name = file.Name.Split('\\');
+            var name = ($"{ file_name[file_name.Length - 1]}");
+            var mimeType = "plain/text";
+
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = name,
+                MimeType = mimeType,
+                Parents = new[] { documentId }
+            };
+
+            FilesResource.CreateMediaUpload request;
+            using (var stream = file)
+            {
+                request = service.Files.Create(fileMetadata, stream, mimeType);
+                request.Fields = "id, name, parents, createdTime, modifiedTime, mimeType, thumbnailLink";
+                await request.UploadAsync();
+            }
+
+
+            return request.ResponseBody;
+        }
+       
+    }
+}
